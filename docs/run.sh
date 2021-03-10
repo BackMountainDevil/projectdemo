0df6b4935664432d88139b8bcec4e4c4a244ad65
kearney <191615342@qq.com>
2021-03-10 18:03:32 +0800

    add docsify's way

--- docs/run.sh
+++ docs/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # FileName: run.sh
 # Daec: Dect the version of python and star a web server for docsify
+#       if theie is no python, it will try docsify
 
 PY_VERSION=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $1}'`
 echo "Your Python Version: " $PY_VERSION
@@ -12,4 +13,7 @@ then
 elif (( $PY_VERSION == 2 ))
 then
     python -m SimpleHTTPServer 3000
+else
+    echo "Fail to get the version of your pyhon. Trying it with docsify"
+    docsify serve docs
 fi
